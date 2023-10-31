1a3cda0c95446596a55512f88b0af6cb9345e9ab
Sam Luong <sam.luong1@education.nsw.gov.au>
2023-11-01 10:21:33 +1100

    Joystick updated with camera mode

--- DysPad/sl.functions.ino
+++ DysPad/sl.functions.ino
@@ -1,5 +1,6 @@
 #define VRX_PIN  A0 // Arduino pin connected to VRX pin
 #define VRY_PIN  A1 // Arduino pin connected to VRY pin
+#define SWITCH_PIN 2 // Arduino pin connected to the switch
 
 #define LEFT_THRESHOLD  400
 #define RIGHT_THRESHOLD 800
@@ -15,35 +16,70 @@
 int xValue = 0 ; // To store value of the X axis
 int yValue = 0 ; // To store value of the Y axis
 int command = COMMAND_NO;
+bool cameraMode = false; // Variable to track the camera mode
 
 void setup() {
   Serial.begin(9600);
+  pinMode(SWITCH_PIN, INPUT_PULLUP); // Set the switch pin as an input with internal pull-up resistor
 }
 
 void performGameAction(int action) {
   // Add code here to perform game actions based on the command
-  switch (action) {
-    case COMMAND_LEFT:
-      Serial.println("Performing LEFT action");
-      // Implement the left movement in your game
-      break;
-    case COMMAND_RIGHT:
-      Serial.println("Performing RIGHT action");
-      // Implement the right movement in your game
-      break;
-    case COMMAND_UP:
-      Serial.println("Performing UP action");
-      // Implement the up movement in your game
-      break;
-    case COMMAND_DOWN:
-      Serial.println("Performing DOWN action");
-      // Implement the down movement in your game
-      break;
-    // Add more cases for other commands if needed
+  if (cameraMode) {
+    // Camera direction movement
+    switch (action) {
+      case COMMAND_LEFT:
+        Serial.println("Camera LEFT");
+        // Implement camera movement to the left
+        break;
+      case COMMAND_RIGHT:
+        Serial.println("Camera RIGHT");
+        // Implement camera movement to the right
+        break;
+      case COMMAND_UP:
+        Serial.println("Camera UP");
+        // Implement camera movement upwards
+        break;
+      case COMMAND_DOWN:
+        Serial.println("Camera DOWN");
+        // Implement camera movement downwards
+        break;
+      // Add more cases for other commands if needed
+    }
+  } else {
+    // Normal joystick movement
+    switch (action) {
+      case COMMAND_LEFT:
+        Serial.println("Character LEFT");
+        // Implement character movement to the left
+        break;
+      case COMMAND_RIGHT:
+        Serial.println("Character RIGHT");
+        // Implement character movement to the right
+        break;
+      case COMMAND_UP:
+        Serial.println("Character UP");
+        // Implement character movement upwards
+        break;
+      case COMMAND_DOWN:
+        Serial.println("Character DOWN");
+        // Implement character movement downwards
+        break;
+      // Add more cases for other commands if needed
+    }
   }
 }
 
 void loop() {
+  // Read the state of the switch
+  bool switchState = digitalRead(SWITCH_PIN);
+
+  // If the switch is pressed, toggle the camera mode
+  if (!switchState) {
+    cameraMode = !cameraMode;
+    delay(250); // Add a small delay to debounce the switch
+  }
+
   // Read analog X and Y analog values
   xValue = analogRead(VRX_PIN);
   yValue = analogRead(VRY_PIN);
@@ -85,4 +121,3 @@ void loop() {
     performGameAction(COMMAND_DOWN);
   }
 }
-
