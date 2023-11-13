9d85476bf48183bfcfea92713c12e015cb3f37c4
RomanLac <roman.lacbungan@education.nsw.gov.au>
2023-11-13 12:25:49 +1100

    removed quotations in KEY_ESC

--- DysPad/rl.functions.ino
+++ DysPad/rl.functions.ino
@@ -1,13 +1,12 @@
-// libaries being used: Keyboard
-
 #include <Keyboard.h>
 
-unsigned int BUTTON_PIN1 = D5;
-unsigned int BUTTON_PIN2 = D6;
-unsigned int BUTTON_PIN3 = D7;
-unsigned int BUTTON_PIN4 = D8;
+static unsigned int BUTTON_PIN1 = D5;
+static unsigned int BUTTON_PIN2 = D6;
+static unsigned int BUTTON_PIN3 = D7;
+static unsigned int BUTTON_PIN4 = D8;
 
 void setup() {
+  Serial.begin(9600);
   Keyboard.begin();
 
   pinMode(BUTTON_PIN1, INPUT_PULLUP);
@@ -17,40 +16,28 @@ void setup() {
 }
 
 void loop() {
-
-  // combo which is quick restart in-game
-  if (digitalRead(BUTTON_PIN1) == LOW + digitalRead(BUTTON_PIN2)) {
-  Keyboard.press('R');
-  } else {
-  Keyboard.release('R');
-  }
-
-  // dash
   if (digitalRead(BUTTON_PIN1) == LOW) {
     Keyboard.press('X');
   } else {
     Keyboard.release('X');
   }
 
-  // grab or interact
   if (digitalRead(BUTTON_PIN2) == LOW) {
     Keyboard.press('Z');
   } else {
     Keyboard.release('Z');
   }
   
-  // jump
   if (digitalRead(BUTTON_PIN3) == LOW) {
     Keyboard.press('C');
   } else {
     Keyboard.release('C');
   }
 
-  // pause
   if (digitalRead(BUTTON_PIN4) == LOW) {
-    Keyboard.press('KEY_ESC');
+    Keyboard.press(KEY_ESC);
   } else {
-    Keyboard.release('KEY_ESC');
+    Keyboard.release(KEY_ESC);
   }
 
   delay(100);
