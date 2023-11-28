9647a2e75c33fac1a0fa9e53d0dada249d946700
Sam Luong <sam.luong1@education.nsw.gov.au>
2023-11-29 09:54:15 +1100

    JOYSTICK CODE UPDATE

--- DysPad/sl.functions.ino
+++ DysPad/sl.functions.ino
@@ -1,142 +1,174 @@
 #include <Mouse.h>
 #include <Keyboard.h>
 
-#define VRX_PIN  A0 // Arduino pin connected to VRX pin
-#define VRY_PIN  A1 // Arduino pin connected to VRY pin
-#define SWITCH_PIN 2 // Arduino pin connected to the switch
+#define VRX_PIN A0
+#define VRY_PIN A1
+#define SWITCH_PIN 2
 
 #define LEFT_THRESHOLD  400
+#define LEFTUP_THRESHOLD  400
+#define LEFTDOWN_THRESHOLD  400
 #define RIGHT_THRESHOLD 800
-#define UP_THRESHOLD    400
-#define DOWN_THRESHOLD  800
+#define RIGHTUP_THRESHOLD 800
+#define RIGHTDOWN_THRESHOLD 800
+#define UP_THRESHOLD 400
+#define DOWN_THRESHOLD 800
 
-#define COMMAND_NO     0x00
-#define COMMAND_LEFT   0x01
-#define COMMAND_RIGHT  0x02
-#define COMMAND_UP     0x04
-#define COMMAND_DOWN   0x08
+#define COMMAND_NO 0x00
+#define COMMAND_LEFT 0x01
+#define COMMAND_LEFTUP 0x02
+#define COMMAND_LEFTDOWN 0x03
+#define COMMAND_RIGHT 0x04
+#define COMMAND_RIGHTUP 0x05
+#define COMMAND_RIGHTDOWN 0x06
+#define COMMAND_UP 0x07
+#define COMMAND_DOWN 0x08
 
-int xValue = 0; // To store value of the X axis
-int yValue = 0; // To store value of the Y axis
+int xValue = 0;
+int yValue = 0;
 int command = COMMAND_NO;
-bool cameraMode = false; // Variable to track the camera mode
+bool cameraMode = false;
 
 void performGameAction(int action) {
-  // Add code here to perform game actions based on the command
   if (cameraMode) {
-    // Camera direction movement
     switch (action) {
       case COMMAND_LEFT:
         Serial.println("Camera LEFT");
-        // Implement camera movement to the left
-        Mouse.move(-10, 0); // Mouse cursor move to the left 10 pixels
+        Mouse.move(-10, 0);
+        break;
+      case COMMAND_LEFTUP:
+        Serial.println("Camera LEFT UP");
+        Mouse.move(-10, 10);
+        break;
+      case COMMAND_LEFTDOWN:
+        Serial.println("Camera LEFT DOWN");
+        Mouse.move(-10, -10);
         break;
       case COMMAND_RIGHT:
         Serial.println("Camera RIGHT");
-        // Implement camera movement to the right
-        Mouse.move(10, 0); // Mouse cursor move to the right 10 pixels
+        Mouse.move(10, 0);
+        break;
+      case COMMAND_RIGHTUP:
+        Serial.println("Camera RIGHT UP");
+        Mouse.move(10, 10);
+        break;
+      case COMMAND_RIGHTDOWN:
+        Serial.println("Camera RIGHT DOWN");
+        Mouse.move(10, -10);
         break;
       case COMMAND_UP:
         Serial.println("Camera UP");
-        // Implement camera movement upwards
-        Mouse.move(0, -10); // Mouse cursor move up 10 pixels
+        Mouse.move(0, -10);
         break;
       case COMMAND_DOWN:
         Serial.println("Camera DOWN");
-        // Implement camera movement downwards
-        Mouse.move(0, 10); // Mouse cursor move down 10 pixels
+        Mouse.move(0, 10);
         break;
-      // Add more cases for other commands if needed
     }
   } else {
-    // Normal joystick movement
     switch (action) {
       case COMMAND_LEFT:
         Serial.println("Character LEFT");
-        // Implement character movement to the left
-        Keyboard.press('A'); // Simulate pressing the 'A' key
-        delay(50); // Add a small delay to hold the key
-        Keyboard.release('A'); // Release the 'A' key
+        Keyboard.press('A');
+        delay(50);
+        Keyboard.release('A');
+        break;
+      case COMMAND_LEFTUP:
+        Serial.println("Character LEFTUP");
+        Keyboard.press('W');
+        delay(50);
+        Keyboard.release('W');
+        break;
+      case COMMAND_LEFTDOWN:
+        Serial.println("Character LEFTDOWN");
+        Keyboard.press('S');
+        delay(50);
+        Keyboard.release('S');
         break;
       case COMMAND_RIGHT:
         Serial.println("Character RIGHT");
-        // Implement character movement to the right
-        Keyboard.press('D'); // Simulate pressing the 'D' key
-        delay(50); // Add a small delay to hold the key
-        Keyboard.release('D'); // Release the 'D' key
+        Keyboard.press('D');
+        delay(50);
+        Keyboard.release('D');
+        break;
+      case COMMAND_RIGHTUP:
+        Serial.println("Character RIGHTUP");
+        Keyboard.press('W');
+        Keyboard.press('D');
+        delay(50);
+        Keyboard.release('W');
+        Keyboard.release('D');
+        break;
+      case COMMAND_RIGHTDOWN:
+        Serial.println("Character RIGHTDOWN");
+        Keyboard.press('S');
+        Keyboard.press('D');
+        delay(50);
+        Keyboard.release('S');
+        Keyboard.release('D');
         break;
       case COMMAND_UP:
         Serial.println("Character UP");
-        // Implement character movement upwards
-        Keyboard.press('W'); // Simulate pressing the 'W' key
-        delay(50); // Add a small delay to hold the key
-        Keyboard.release('W'); // Release the 'W' key
+        Keyboard.press('W');
+        delay(50);
+        Keyboard.release('W');
         break;
       case COMMAND_DOWN:
         Serial.println("Character DOWN");
-        // Implement character movement downwards
-        Keyboard.press('S'); // Simulate pressing the 'S' key
-        delay(50); // Add a small delay to hold the key
-        Keyboard.release('S'); // Release the 'S' key
+        Keyboard.press('S');
+        delay(50);
+        Keyboard.release('S');
         break;
-      // Add more cases for other commands if needed
     }
   }
 }
 
 void setup() {
   Serial.begin(9600);
-  pinMode(SWITCH_PIN, INPUT_PULLUP); // Set the switch pin as an input with an internal pull-up resistor
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  // Read the state of the switch
   bool switchState = digitalRead(SWITCH_PIN);
 
-  // If the switch is pressed, toggle the camera mode
   if (!switchState) {
     cameraMode = !cameraMode;
-    delay(250); // Add a small delay to debounce the switch
+    delay(250);
   }
 
-  // Read analog X and Y analog values
   xValue = analogRead(VRX_PIN);
   yValue = analogRead(VRY_PIN);
 
-  // Converts the analog value to commands
-  // Reset commands
   command = COMMAND_NO;
 
-  // Check left/right commands
   if (xValue < LEFT_THRESHOLD)
     command = command | COMMAND_LEFT;
   else if (xValue > RIGHT_THRESHOLD)
     command = command | COMMAND_RIGHT;
 
-  // Check up/down commands
+  if (xValue < LEFTUP_THRESHOLD)
+    command = command | COMMAND_LEFTUP;
+  else if (xValue > RIGHTUP_THRESHOLD)
+    command = command | COMMAND_RIGHTUP;
+
+  if (xValue < LEFTDOWN_THRESHOLD)
+    command = command | COMMAND_LEFTDOWN;
+  else if (xValue > RIGHTDOWN_THRESHOLD)
+    command = command | COMMAND_RIGHTDOWN;
+
   if (yValue < UP_THRESHOLD)
     command = command | COMMAND_UP;
   else if (yValue > DOWN_THRESHOLD)
     command = command | COMMAND_DOWN;
 
-  // Print command to serial and process command
-  if (command & COMMAND_LEFT) {
-    Serial.println("COMMAND LEFT");
-    performGameAction(COMMAND_LEFT);
-  }
+  Serial.print("X: ");
+  Serial.print(xValue);
+  Serial.print("\tY: ");
+  Serial.print(yValue);
+  Serial.print("\tCommand: ");
+  Serial.println(command, HEX);
 
-  if (command & COMMAND_RIGHT) {
-    Serial.println("COMMAND RIGHT");
-    performGameAction(COMMAND_RIGHT);
-  }
-
-  if (command & COMMAND_UP) {
-    Serial.println("COMMAND UP");
-    performGameAction(COMMAND_UP);
-  }
-
-  if (command & COMMAND_DOWN) {
-    Serial.println("COMMAND DOWN");
-    performGameAction(COMMAND_DOWN);
+  if (command != COMMAND_NO) {
+    performGameAction(command);
   }
 }
